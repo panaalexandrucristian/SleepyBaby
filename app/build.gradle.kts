5d0a4aaff52ff32dfa7dd72de0d7746ddbaf3106
Alexandru-Cristian Pana <pana.alexandru.cristian@gmail.com>
2025-09-07 10:52:39 +0300

    set compile and target sdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ro.pana.sleepybaby"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ro.pana.sleepybaby"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
